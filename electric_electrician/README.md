9c0b09f820fdffcd29b22f4a12f5ff84aec5e239
ceoDemitri <demichole@gmail.com>
2026-03-18 23:32:43 -0500

    refactor: updates the styles for hero section on landing page. groups related form fieldsets. adds one  more task on readme, hero and nav final draft

--- electric_electrician/README.md
+++ electric_electrician/README.md
@@ -8,7 +8,9 @@ clients will be able to book a consultation, browse services and see ratings and
 
 ## todo
 
-- [ ] fieldset: label vs legend? okay to use both? may swap?
+- [ x ] fieldset: label vs legend? okay to use both? may swap?
+  -- legend is for a collection like radio items; label is for a specific label for a single form control
+- [ ] hero and navbar final draft
 - [ ] value section: reviews, sales, stats, our value
 
 -- why did i omit tailwind in this journey. i wonder
--- electric_electrician/src/routes/+page.svelte
+++ electric_electrician/src/routes/+page.svelte
@@ -4,60 +4,65 @@
 <div class="page">
 	<header>
 		<article>
-			<h1>The Finest <span>Kinetic</span> Output Seen this Lifetime</h1>
+			<h1>The Finest <span class="kinetic">Kinetic</span> Output Seen this Lifetime</h1>
 			<p>
 				Your trusted orgon partner for all electrical needs. We provide top-notch services to ensure
-				your home and business are powered safely and efficiently.
+				your home and business are powered safely and efficiently. Explore our services and get in
+				touch with us for reliable electrical solutions.
 			</p>
-			<p>Explore our services and get in touch with us for reliable electrical solutions.</p>
 		</article>
 
 		<section class="book-consult">
 			<h2>Book A Consultation today</h2>
 
-			<form method="post">
-				<fieldset>
-					<legend>Service Type</legend>
-					<label>
-						<input type="radio" name="service" value="residential" required />
-						Residential
-					</label>
-					<label>
-						<input type="radio" name="service" value="commercial" required />
-						Commercial
-					</label>
-					<label>
-						<input type="radio" name="service" value="industrial" required />
-						Industrial
-					</label>
-				</fieldset>
-				<fieldset>
-					<legend>Service Required</legend>
-					<label>
-						<input type="checkbox" name="service_required" value="installation" />
-						Installation
-					</label>
-					<label>
-						<input type="checkbox" name="service_required" value="repair" />
-						Repair
-					</label>
-					<label>
-						<input type="checkbox" name="service_required" value="maintenance" />
-						Maintenance
-					</label>
-				</fieldset>
-				<fieldset>
-					<legend>Personal Information</legend>
-					<label for="name">Name:</label>
-					<input type="text" id="name" name="name" required />
-				</fieldset>
-				<fieldset>
-					<legend>Contact Information</legend>
-					<label for="email">Email Address:</label>
-					<input type="email" id="email" name="email" required />
-				</fieldset>
+			<form method="post" class="main-form">
+				<div class="user-type">
+					<fieldset>
+						<legend>Service Type</legend>
+						<label>
+							<input type="radio" name="service" value="residential" required />
+							Residential
+						</label>
+						<label>
+							<input type="radio" name="service" value="commercial" required />
+							Commercial
+						</label>
+						<label>
+							<input type="radio" name="service" value="industrial" required />
+							Industrial
+						</label>
+					</fieldset>
+					<fieldset>
+						<legend>Service Required</legend>
+						<label>
+							<input type="checkbox" name="service_required" value="installation" />
+							Installation
+						</label>
+						<label>
+							<input type="checkbox" name="service_required" value="repair" />
+							Repair
+						</label>
+						<label>
+							<input type="checkbox" name="service_required" value="maintenance" />
+							Maintenance
+						</label>
+					</fieldset>
+				</div>
 
-				<div>
+				<div class="user-info">
+					<fieldset>
+						<legend>Personal Information</legend>
+						<label for="name">Name:</label>
+						<input type="text" id="name" name="name" required />
+					</fieldset>
+					<fieldset>
+						<legend>Contact Information</legend>
+						<label for="email">Email Address:</label>
+						<input type="email" id="email" name="email" required />
+					</fieldset>
+				</div>
+
+				<div class="user-date">
 					<fieldset>
 						<label for="date">Choose Date:</label>
 						<input type="date" id="date" name="date" required />
@@ -67,8 +72,9 @@
 						<input type="time" id="time" name="time" required />
 					</fieldset>
 				</div>
+
 				<fieldset>
-					<legend>City</legend>
+					<legend>Address</legend>
 					<input type="text" name="location" required />
 				</fieldset>
 
@@ -79,6 +85,27 @@
 </div>
 
 <style>
+	h1 {
+		font-size: 2.8rem;
+		margin-bottom: 1rem;
+		width: 25ch;
+	}
+
+	.kinetic {
+		color: #aaae67;
+		font-weight: bold;
+		font-style: italic;
+	}
+
+	article p {
+		font-size: 0.95rem;
+		line-height: 1.5;
+		max-width: 500px;
+		text-align: justify;
+	}
+
+	/* --------------------------- */
+	/* form */
 	.book-consult {
 		margin-top: 2rem;
 		padding: 1rem;
@@ -86,6 +113,7 @@
 		border-radius: 8px;
 		display: flex;
 		flex-direction: column;
+		width: 60vw;
 	}
 
 	.book-consult form {
@@ -93,14 +121,52 @@
 		flex-direction: column;
 	}
 
-	button {
+	.main-form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	fieldset {
+		border: 1px solid #ccc;
+		border-radius: 8px;
+		padding: 1rem;
+		margin-bottom: 1rem;
+		width: 50%;
+	}
+
+	input[type='text'],
+	input[type='email'] {
+		width: 90%;
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+	}
+
+	.user-type,
+	.user-info,
+	.user-date {
+		display: flex;
+		gap: 2rem;
+	}
+
+	form button {
 		margin-top: 1rem;
 		padding: 0.5rem 1rem;
-		background-color: #007bff;
-		color: white;
+		background-color: #d4dc3ba7;
+		color: rgb(0, 0, 0);
 		border: none;
 		border-radius: 4px;
 		cursor: pointer;
 		display: block;
+		text-transform: uppercase;
+		font-weight: bold;
+		transition: background-color 0.3s ease;
+		letter-spacing: 1.5em;
+	}
+
+	form button:hover {
+		background-color: #aaae67;
+		color: #fff;
 	}
 </style>
